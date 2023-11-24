d9b4fae4c524c3ec2ae1db0511204df425d6e6b1
Anatoly Politsin <politsin@gmail.com>
2023-11-24 14:01:41 +0300

    mmost

--- mattermost/build.sh
+++ mattermost/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-docker build -t synstd/mattermost .
-docker tag synstd/mattermost synstd/mattermost:9.2.2
-# docker login --username=synstd
-# тут нужно ввести пароль
+# docker build -t synstd/mattermost .
+# docker tag synstd/mattermost synstd/mattermost:9.2.2
+# # docker login --username=synstd
+# # тут нужно ввести пароль
 
-docker push synstd/mattermost
+# docker push synstd/mattermost
+# docker push synstd/mattermost:9.2.2
+
+docker build -t synstd/mattermost:9.2.2 .
 docker push synstd/mattermost:9.2.2
--- php-service/build.sh
+++ php-service/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-VERSION="8.1"
+VERSION="8.2"
 BUILD="0"
 
-# cd /opt/build/docker-php
+#docker push synstd/php-service
 docker build . -t synstd/php-service
 docker tag synstd/php-service synstd/php-service:$VERSION
 docker tag synstd/php-service synstd/php-service:$VERSION.$BUILD
-docker push synstd/php-service
 docker push synstd/php-service:$VERSION
 docker push synstd/php-service:$VERSION.$BUILD
