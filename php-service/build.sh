8e41f5e26d6696bde6aa06ff5aeb938b270730cf
Anatoly Politsin <politsin@gmail.com>
2023-11-27 01:05:53 +0300

    manifest

--- php-service/build.sh
+++ php-service/build.sh
@@ -6,6 +6,8 @@ BUILD="0"
 #docker push synstd/php-service
 docker build . -t synstd/php-service
 docker tag synstd/php-service synstd/php-service:$VERSION
+docker tag synstd/php-service synstd/php-service:$VERSION-amd64
 docker tag synstd/php-service synstd/php-service:$VERSION.$BUILD
 docker push synstd/php-service:$VERSION
+docker push synstd/php-service:$VERSION-amd64
 docker push synstd/php-service:$VERSION.$BUILD
---
+++ php-service/manifest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+VERSION="8.2"
+
+docker manifest create synstd/php-service:$VERSION \
+--amend synstd/php-service:$VERSION-amd64 \
+--amend synstd/php-service:$VERSION-arm
