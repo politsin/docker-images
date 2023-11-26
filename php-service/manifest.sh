a93214c1a3a9486b45aa64a4d609fc540086d4a1
Anatoly Politsin <politsin@gmail.com>
2023-11-27 02:08:43 +0300

    manifest

--- php-service/manifest.sh
+++ php-service/manifest.sh
@@ -5,3 +5,5 @@ VERSION="8.2"
 docker manifest create synstd/php-service:$VERSION \
 --amend synstd/php-service:$VERSION-amd64 \
 --amend synstd/php-service:$VERSION-arm
+
+docker manifest push synstd/php-service:$VERSION
