c84f79c49d84d0d19cc88f11183b4c30f6d6649f
Anatoly <politsin@gmail.com>
2023-11-29 10:27:18 +0300

    Update build.sh

--- php/build.sh
+++ php/build.sh
@@ -8,7 +8,9 @@ if docker build . -t synstd/php ; then
   docker tag synstd/php synstd/php:$VERSION
   docker tag synstd/php synstd/php:$VERSION-amd64
   docker tag synstd/php synstd/php:$VERSION.$BUILD
-  docker push synstd/php:$VERSION
+
+  # Push to https://hub.docker.com/r/synstd/php
   docker push synstd/php:$VERSION-amd64
   docker push synstd/php:$VERSION.$BUILD
+  docker push synstd/php:$VERSION
 fi
