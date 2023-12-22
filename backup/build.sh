7726e51fd45c4b715edc16b3245c32c2b4e51b97
Anatoly Politsin <politsin@gmail.com>
2023-12-22 08:38:15 +0000

    dockup

--- backup/build.sh
+++ backup/build.sh
@@ -10,4 +10,5 @@ if docker build . -t synstd/s3-dockup ; then
   # Push to https://hub.docker.com/r/synstd/s3-dockup
   docker push synstd/s3-dockup:$VERSION.$BUILD
   docker push synstd/s3-dockup:$VERSION
+  docker push synstd/s3-dockup
 fi
