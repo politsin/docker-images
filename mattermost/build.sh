8064a2c5716101daee2bbe2176ea5f7a9b57d910
Anatoly Politsin <politsin@gmail.com>
2024-05-08 09:41:18 +0300

    mmost

--- mattermost/build.sh
+++ mattermost/build.sh
@@ -4,6 +4,6 @@
 # тут нужно ввести пароль
 
 docker build -t synstd/mattermost .
-docker tag synstd/mattermost synstd/mattermost:9.2.2
+docker tag synstd/mattermost synstd/mattermost:9.7.3
 docker push synstd/mattermost
-docker push synstd/mattermost:9.2.2
+docker push synstd/mattermost:9.7.3
