e5ce9a823511e78ff03ef2173797f8d0109882fd
Anatoly Politsin <politsin@gmail.com>
2025-08-23 12:20:31 +0300

    mmost curl

--- mattermost/build.sh
+++ mattermost/build.sh
@@ -3,7 +3,10 @@
 # docker login --username=synstd
 # тут нужно ввести пароль
 
-docker build -t synstd/mattermost .
-docker tag synstd/mattermost synstd/mattermost:10.9.2
+docker build --network=host -t synstd/mattermost . || { 
+    echo "[!] Ошибка: сборка не удалась"; 
+    exit 1; 
+}
+docker tag synstd/mattermost synstd/mattermost:10.11.2
 docker push synstd/mattermost
-docker push synstd/mattermost:10.9.2
+docker push synstd/mattermost:10.11.2
