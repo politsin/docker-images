75a44ef522007450f5138f6bc741b6814d9a5f68
Anatoly Politsin <politsin@gmail.com>
2025-09-04 08:37:18 +0300

    exim-update

--- exim/build.sh
+++ exim/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker build -t synstd/exim .
-docker tag synstd/exim synstd/exim:4.96.2
+docker tag synstd/exim synstd/exim:4.98.2
 # docker login --username=synstd
 # тут нужно ввести пароль
 
 docker push synstd/exim
-docker push synstd/exim:4.96.2
+docker push synstd/exim:4.98.2
