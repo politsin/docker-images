3d447d17bd68848fe19f60f773b7691a312e8b6d
Anatoly Politsin <politsin@gmail.com>
2025-10-03 12:23:47 +0300

    backup. remove composer install on backup restore

--- backup/console/src/Step/DownloadBackupUntarStep.php
+++ backup/console/src/Step/DownloadBackupUntarStep.php
@@ -15,8 +15,7 @@ class DownloadBackupUntarStep extends StepBase {
     $options = $_ENV['BACKUP_TAR_OPTION_RESTORE'] ?? '';
     $cmd = "tar xzf $file -C / $options";
     $result = $this->command->runProcess($cmd);
-    $composer = '/usr/local/bin/composer';
-    $result = $this->command->runProcess("$composer install --no-interaction -d /var/www/html");
+
     $this->command->logExecute(
       $result['success'] ?? FALSE,
       'Unpack backup',
