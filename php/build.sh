3aad9043e7f2528a1311ce5f57c101c9ad7a5a85
Anatoly Politsin <politsin@gmail.com>
2026-09-11 23:45:42 +0300

    Update PHP images for Ubuntu 26.04

--- php/build.sh
+++ php/build.sh
@@ -5,13 +5,19 @@ set -euo pipefail
 IMAGE="synstd/php"
 VERSION="${VERSION:-8.4}"
 BUILD="${BUILD:-3}"
+NO_CACHE="${NO_CACHE:-1}"
 
-if docker build --build-arg PHP="$VERSION" . -t "${IMAGE}" ; then
-  docker tag "${IMAGE}" "${IMAGE}:${VERSION}"
-  docker tag "${IMAGE}" "${IMAGE}:${VERSION}-amd64"
-  docker tag "${IMAGE}" "${IMAGE}:${VERSION}.${BUILD}"
-
-  docker push "${IMAGE}:${VERSION}-amd64"
-  docker push "${IMAGE}:${VERSION}.${BUILD}"
-  docker push "${IMAGE}:${VERSION}"
+BUILD_FLAGS=(--pull --build-arg PHP="$VERSION")
+if [ "$NO_CACHE" = "1" ]; then
+  BUILD_FLAGS+=(--no-cache)
 fi
+
+docker build "${BUILD_FLAGS[@]}" . -t "${IMAGE}"
+
+docker tag "${IMAGE}" "${IMAGE}:${VERSION}"
+docker tag "${IMAGE}" "${IMAGE}:${VERSION}-amd64"
+docker tag "${IMAGE}" "${IMAGE}:${VERSION}.${BUILD}"
+
+docker push "${IMAGE}:${VERSION}-amd64"
+docker push "${IMAGE}:${VERSION}.${BUILD}"
+docker push "${IMAGE}:${VERSION}"
